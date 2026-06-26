06213bd4e96c3eea7d06a00957307d17d20c114b
huangch <2898938+huangch@users.noreply.github.com>
2026-06-25 22:15:00 -0700

    modified:   sptxinsight/_version.py

--- sptxinsight/_version.py
+++ sptxinsight/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.0'
-__version_tuple__ = version_tuple = (0, 1, 0)
+__version__ = version = '0.1.1.dev13+ge26ff3823.d20260626'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev13', 'ge26ff3823.d20260626')
 
-__commit_id__ = commit_id = None
+__commit_id__ = commit_id = 'ge26ff3823'
