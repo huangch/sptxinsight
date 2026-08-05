29f17275861d9e34322cb2a0424969f721be23f5
huangch <2898938+huangch@users.noreply.github.com>
2026-08-04 20:44:42 -0700

    modified:   pyproject.toml 	modified:   sptxinsight/_version.py

--- sptxinsight/_version.py
+++ sptxinsight/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.1.dev33+g37ee1ef2a.d20260731'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev33', 'g37ee1ef2a.d20260731')
+__version__ = version = '0.1.1.dev36+gd184ae953.d20260805'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev36', 'gd184ae953.d20260805')
 
-__commit_id__ = commit_id = 'g37ee1ef2a'
+__commit_id__ = commit_id = 'gd184ae953'
